87f7e5c7487c0763e4ec3d26dc5aacdb28c33a63
Rafael Goulart <Rafael.Goulart2@etecia.com.br>
2020-09-02 23:40:08 -0300

    3rd Commit Adicionado:  - Melhora no código  - Math.ceil

--- src/Exercicio0706/Ext.java
+++ src/Exercicio0706/Ext.java
@@ -5,6 +5,15 @@ import javax.swing.*;
 public class Ext {
 
     public static void saidaUsuario(double salBruto, double aliquota, double desconto, double salLiq) {
-        JOptionPane.showMessageDialog(null, "Salário bruto: " + salBruto + "\n Alíquota INSS: " + aliquota + "\n Desconto: " + desconto + "\n Salário líquido: " + salLiq, "Desconto INSS", JOptionPane.INFORMATION_MESSAGE);
-    }   
+
+        desconto = desconto * 100;
+        salLiq = salLiq * 100;
+        desconto = Math.ceil(desconto);
+        salLiq = Math.ceil(salLiq);
+        desconto = desconto / 100;
+        salLiq = salLiq / 100;
+
+        JOptionPane.showMessageDialog(null, "Salário bruto: " + salBruto + "\n Alíquota INSS: " + aliquota + "\n Desconto: " + desconto + "\n Salário líquido: " + salLiq, "Recebimento", JOptionPane.INFORMATION_MESSAGE);
+
+    }
 }
--- src/Exercicio0706/Salario.java
+++ src/Exercicio0706/Salario.java
@@ -3,45 +3,46 @@ package Exercicio0706;
 import javax.swing.*;
 
 public class Salario {
-    
+
     public static void main(String[] args) {
-        
-        double salLiq = 0, salBruto = 0, aliquota = 0, desconto = 0, salMax = 354.07;
+
+        double salLiq = 0, salBruto = 0, aliquota = 0, desconto = 0;
         String aux = " ";
-        
+
         aux = JOptionPane.showInputDialog(null, "Insira o seu salário", "Desconto INSS", JOptionPane.PLAIN_MESSAGE);
         aux = aux.replace(',', '.');
         salBruto = Double.parseDouble(aux);
-        
+
         if (salBruto < 465.00 || salBruto > 3218.90) {
-            
+
             JOptionPane.showMessageDialog(null, "Não é possível fazer o desconto \ncom valor menor que R$465,00 \nou maior que R$3.218,00 ", "Valor inválido", JOptionPane.ERROR_MESSAGE);
         } else if (salBruto <= 965.67) {
-            
+
             aliquota = 8;
             desconto = (salBruto / 100) * aliquota;
             salLiq = salBruto - desconto;
             Ext.saidaUsuario(salBruto, aliquota, desconto, salLiq);
         } else if (salBruto >= 965.68 && salBruto <= 1609.45) {
-            
+
             aliquota = 9;
             desconto = (salBruto / 100) * aliquota;
             salLiq = salBruto - desconto;
             Ext.saidaUsuario(salBruto, aliquota, desconto, salLiq);
         } else if (salBruto >= 1609.46 && salBruto <= 3218.90) {
-            
+
             aliquota = 11;
             desconto = (salBruto / 100) * 11;
-            if (desconto >= salMax) {
-                
-                salLiq = salBruto - salMax;
+            if (desconto >= 354.07) {
+
+                salLiq = 2864.83;
+                desconto = 354.07;
                 Ext.saidaUsuario(salBruto, aliquota, desconto, salLiq);
-            } else {
+            } else if (desconto <= 354.07) {
                 salLiq = salBruto - desconto;
                 Ext.saidaUsuario(salBruto, aliquota, desconto, salLiq);
             }
-            
+
         }
     }
-    
+
 }
